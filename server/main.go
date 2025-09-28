24b2fa18d3fdfedb06fa6149fdf6dc7e95161957
lian <msn19882009@live.cn>
2025-09-28 15:02:36 +0800

    feat: [KAN-42] template init

---
+++ server/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/NUS-ISS-Agile-Team/ceramicraft-mservice-template/server/config"
+	"github.com/NUS-ISS-Agile-Team/ceramicraft-mservice-template/server/grpc"
+	"github.com/NUS-ISS-Agile-Team/ceramicraft-mservice-template/server/http"
+	"github.com/NUS-ISS-Agile-Team/ceramicraft-mservice-template/server/log"
+	"github.com/NUS-ISS-Agile-Team/ceramicraft-mservice-template/server/repository"
+)
+
+var (
+	sigCh = make(chan os.Signal, 1)
+)
+
+func main() {
+	config.Init()
+	log.InitLogger()
+	repository.Init()
+	go grpc.Init(sigCh)
+	go http.Init(sigCh)
+	// listen terminage signal
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	sig := <-sigCh // Block until signal is received
+	log.Logger.Infof("Received signal: %v, shutting down...", sig)
+}
